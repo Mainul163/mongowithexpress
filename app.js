36d85c7671f8cad0cfdd8c44a8dc4f1518259e37
unknown <mainulisraf163@gmail.com>
2023-11-21 17:34:31 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const userRouter = require("./routes/user.route");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use("/api/users", userRouter);
+
+// **** router error *****
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "Route not found",
+  });
+});
+
+// ****** server error ******
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: "Server error",
+  });
+});
+
+module.exports = app;
---
+++ controllers/user.controller.js
@@ -0,0 +1,37 @@
+const getAllUsers = (req, res) => {
+  res.status(200).json({
+    message: "all users",
+  });
+};
+
+const getOneUser = (req, res) => {
+  res.status(200).json({
+    message: "get one user",
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(201).json({
+    message: "user is created",
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(200).json({
+    message: "user is update",
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(200).json({
+    message: "user is deleted",
+  });
+};
+
+module.exports = {
+  getAllUsers,
+  getOneUser,
+  createUser,
+  updateUser,
+  deleteUser,
+};
